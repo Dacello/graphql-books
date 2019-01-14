87d036521d59b5ad379f1fb6d3981e74e08790ab
Daniel Andrews <daniel@revelry.co>
2019-01-14 17:09:58 -0600

    Books via GraphQL

--- server/app.js
+++ server/app.js
@@ -1,27 +1,29 @@
-const express = require('express');
-const graphqlHTTP = require('express-graphql');
+const { ApolloServer } = require('apollo-server');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
-const app = express();
-
 // allow cross-origin requests
-app.use(cors());
+// app.use(cors());
 
 // connect to mlab database
 // make sure to replace my db string & creds with your own
-mongoose.connect('mongodb://ninja:test@ds161148.mlab.com:61148/graphql-ninja')
+mongoose.connect('mongodb://localhost:27017/books_demo')
 mongoose.connection.once('open', () => {
     console.log('conneted to database');
 });
 
-// bind express with graphql
-app.use('/graphql', graphqlHTTP({
-    schema,
-    graphiql: true
-}));
+const server = new ApolloServer({ schema });
 
-app.listen(4000, () => {
-    console.log('now listening for requests on port 4000');
+server.listen().then(({ url }) => {
+    console.log(`🚀  Server ready at ${url}`);
 });
+// // bind express with graphql
+// app.use('/graphql', graphqlHTTP({
+//     schema,
+//     graphiql: true
+// }));
+
+// app.listen(4000, () => {
+//     console.log('now listening for requests on port 4000');
+// });
